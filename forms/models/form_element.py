12f43a57129e0a9bfec966e0aaeece08065b5862
Ankit Dahiya <ankitdahiya95@gmail.com>
2016-11-17 17:27:05 +0530

    Add position attribute to model FormElement

--- forms/models/form_element.py
+++ forms/models/form_element.py
@@ -1,6 +1,8 @@
 from django.db import models
 from workflow import Workflow
 
+from positions.fields import PositionField
+
 class FormElement(models.Model):
     workflow = models.ForeignKey(Workflow, on_delete=models.CASCADE)
     caption = models.CharField(max_length=200)
@@ -12,6 +14,10 @@ class FormElement(models.Model):
         ('text_area', 'Text Area')
     )
     element_type = models.CharField(max_length=50, choices=AVAILABLE_ELEMENT_TYPES)
+    position = PositionField(collection='workflow')
+
+    class Meta(object):
+        ordering = ['position']
 
     def __str__(self):
         return self.caption
