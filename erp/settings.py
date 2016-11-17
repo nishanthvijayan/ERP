57ed206d295bc044bd71bca65a80524d987dde16
Jainendra Mandavi <jainendra16mandavi@gmail.com>
2016-11-18 03:12:18 +0530

    Add missing comma in INSTALLED_APPS list

--- erp/settings.py
+++ erp/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'forms.apps.FormsConfig',
-    'login.apps.LoginConfig'
+    'login.apps.LoginConfig',
     'positions',
 ]
 
