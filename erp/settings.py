2d93d790859c72c1e3a6a599a1826b4bae013b81
Jainendra Mandavi <jainendra16mandavi@gmail.com>
2016-11-19 03:32:55 +0530

    Rename LoginConfig to HomeConfig class

--- erp/settings.py
+++ erp/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'forms.apps.FormsConfig',
-    'home.apps.LoginConfig',
+    'home.apps.HomeConfig',
     'positions',
 ]
 
--- home/apps.py
+++ home/apps.py
@@ -3,5 +3,5 @@ from __future__ import unicode_literals
 from django.apps import AppConfig
 
 
-class LoginConfig(AppConfig):
+class HomeConfig(AppConfig):
     name = 'home'
