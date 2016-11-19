eedfe6e459d88cdf7e1b18ad8a6c2345284e79fd
Nishanth Vijayan <nishanththegr8@gmail.com>
2016-11-19 22:26:48 +0530

    Add new pages that lists forms pending approval and current user's forms

--- forms/views/workflow_entry.py
+++ forms/views/workflow_entry.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.contrib import messages
+from django.contrib.auth.models import Group
 
-from forms.models import WorkflowEntry, Workflow
+from forms.models import WorkflowEntry, Workflow, Transition, State
 
 
 @login_required
-def workflow_entry_index(request, workflow_id):
+def workflow_entry_admin_index(request, workflow_id):
     workflow = get_object_or_404(Workflow, pk=workflow_id)
     form_elements = workflow.formelement_set.all()
 
@@ -21,7 +23,14 @@ def workflow_entry_index(request, workflow_id):
         responses = paginator.page(paginator.num_pages)
 
     context = {'workflow': workflow, 'form_elements': form_elements, "responses": responses}
-    return render(request, 'forms/workflow_entries/index.html', context)
+    return render(request, 'forms/workflow_entries/admin_index.html', context)
+
+
+@login_required
+def workflow_entry_user_index(request):
+    responses = WorkflowEntry.objects.filter(creator=request.user)
+    context = {"responses": responses}
+    return render(request, 'forms/workflow_entries/user_index.html', context)
 
 
 @login_required
@@ -39,9 +48,39 @@ def workflow_entry_new(request, workflow_id):
         for form_element in form_elements:
                 submission.formelemententry_set.create(
                     form_element=form_element, value=request.POST[form_element.caption])
-        # TODO: After collecting reponses the page should ideally redirect to a
-        # page listing current user's all submissions
-        return redirect('forms:workflow-index')
+
+        return redirect('forms:workflow-entry-user-index')
 
     context = {'workflow': workflow, 'form_elements': form_elements}
     return render(request, 'forms/workflow_entries/new.html', context)
+
+
+@login_required
+def workflow_entry_pending_index(request):
+    current_user_groups = request.user.groups.values_list('id', flat=True)
+    bad_group_names = list(Group.objects.exclude(id__in=current_user_groups).values_list("name", flat=True))
+    good_states = State.objects.exclude(
+        allowed_groups__name__in=bad_group_names).filter(kind__in=['Initial', 'Intermediate'])
+    responses = WorkflowEntry.objects.filter(current_state__in=good_states)
+
+    context = {"responses": responses}
+    return render(request, 'forms/workflow_entries/pending_index.html', context)
+
+
+@login_required
+def workflow_entry_change_state(request):
+    if request.method == 'POST':
+        users_groups = request.user.groups.values_list("name", flat=True)
+        workflow_entry_id = request.POST['workflow_entry_id']
+        workflow_entry = get_object_or_404(WorkflowEntry, pk=workflow_entry_id)
+
+        if len(workflow_entry.current_state.allowed_groups.filter(name__in=users_groups)) == 0:
+            messages.error(request, 'You are not authorized to perform this action')
+            return redirect('forms:pending')
+
+        transition_id = request.POST['transition_id']
+        transition = get_object_or_404(Transition, pk=transition_id)
+        workflow_entry.current_state = transition.to_state
+        workflow_entry.save()
+
+    return redirect('forms:pending')
