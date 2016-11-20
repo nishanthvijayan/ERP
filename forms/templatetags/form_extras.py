ec32aa459fbf140abfb945618e0eef313eebf956
Nishanth Vijayan <nishanththegr8@gmail.com>
2016-11-20 19:31:06 +0530

    Add values list template tag to display names of allowed groups

---
+++ forms/templatetags/form_extras.py
@@ -0,0 +1,7 @@
+from django import template
+
+register = template.Library()
+
+@register.filter(name='values_list')
+def values_list(query_set, field):
+    return list(query_set.values_list(field, flat=True))
