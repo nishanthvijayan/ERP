05a9e681a058149f69b7941e6104d57a69628c7e
Jainendra Mandavi <jainendra16mandavi@gmail.com>
2017-04-12 01:21:29 +0530

    Add view functions for 3 pages
    3 pages are index, tracking, history

--- reimbursement/views.py
+++ reimbursement/views.py
@@ -1,6 +1,13 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.shortcuts import render
 
-# Create your views here.
+
+def reimbursement_index(request):
+    return render(request, 'reimbursement/index.html')
+
+
+def reimbursement_tracking(request):
+    return render(request, 'reimbursement/tracking.html')
+
+
+def reimbursement_history(request):
+    return render(request, 'reimbursement/history.html')
