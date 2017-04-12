de6dfc695891cc4f2693773e49bb1356c6721536
Jainendra Mandavi <jainendra16mandavi@gmail.com>
2017-04-13 05:00:04 +0530

    Rename track to submissions

--- reimbursement/urls.py
+++ reimbursement/urls.py
@@ -5,7 +5,7 @@ app_name = 'reimbursement'
 
 urlpatterns = [
     url(r'^$', views.reimbursement_index, name='reimbursement-index'),
-    url(r'^track', views.reimbursement_track, name='reimbursement-track'),
+    url(r'^submissions', views.reimbursement_submissions, name='reimbursement-submissions'),
     url(r'^requests/pending', views.reimbursement_requests_pending, name='reimbursement-requests-pending'),
     url(r'^requests/previous', views.reimbursement_requests_previous, name='reimbursement-requests-previous'),
 ]
