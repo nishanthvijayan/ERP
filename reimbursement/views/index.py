8be85420f7f4a3bae19eefac0e80aa787ad92cb4
Jainendra Mandavi <jainendra16mandavi@gmail.com>
2017-04-23 15:33:54 +0530

    Add static view for reimbusrement index

--- reimbursement/views/index.py
+++ reimbursement/views/index.py
@@ -1,11 +1,5 @@
 from django.shortcuts import render
 
-from reimbursement.models.medical import Medical
-
 
 def reimbursement_index(request):
-    medical_list = Medical.objects.all()
-    context = {
-        'medical': medical_list
-    }
-    return render(request, 'reimbursement/index.html', context)
+    return render(request, 'reimbursement/index.html')
