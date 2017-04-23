f919061fc5eb89f871b85b224b166cd51845b695
Jainendra Mandavi <jainendra16mandavi@gmail.com>
2017-04-23 16:23:06 +0530

    Add bootstap paginator

--- reimbursement/views/submissions.py
+++ reimbursement/views/submissions.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
 from reimbursement.models.medical import Medical
 
 
 def reimbursement_submissions(request):
     medical_list = Medical.objects.all()
+    page = request.GET.get('page')
+    paginator = Paginator(medical_list, 10)
+    try:
+        medicals = paginator.page(page)
+    except PageNotAnInteger:
+        medicals = paginator.page(1)
+    except EmptyPage:
+        medicals = paginator.page(paginator.num_pages)
     context = {
-        'medicals': medical_list
+        'medicals': medicals
     }
     return render(request, 'reimbursement/submissions.html', context)
