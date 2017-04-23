ea5d343a9b511e685892f03bbaf315865c0434e8
Jainendra Mandavi <jainendra16mandavi@gmail.com>
2017-04-23 15:31:19 +0530

    Add submissions view to display all entries
    Modify static submission view to generate with real data
    Modify html template to support real data

--- reimbursement/views/submissions.py
+++ reimbursement/views/submissions.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
 
+from reimbursement.models.medical import Medical
+
 
 def reimbursement_submissions(request):
-    return render(request, 'reimbursement/submissions.html')
+    medical_list = Medical.objects.all()
+    context = {
+        'medicals': medical_list
+    }
+    return render(request, 'reimbursement/submissions.html', context)
