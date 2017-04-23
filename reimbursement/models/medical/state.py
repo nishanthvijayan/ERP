79e3fd66cc5fc77f6096d9141005c3eea515bde2
Jainendra Mandavi <jainendra16mandavi@gmail.com>
2017-04-23 16:38:36 +0530

    Add understandable text to django-fsm states

--- reimbursement/models/medical/state.py
+++ reimbursement/models/medical/state.py
@@ -2,14 +2,13 @@ class STATE:
     """
     This stores all the states that the reimbursement form can be in
     """
-    SUBMITTED = 'SUBMITTED'
-    VERIFIED_BY_DA = 'VERIFIED_BY_DA'
-    APPROVED_BY_MS = 'APPROVED_BY_MS'
-    APPROVED_BY_AR = 'APPROVED_BY_AR'
-    APPROVED_BY_DR = 'APPROVED_BY_DR'
-    APPROVED_BY_SrAO = 'APPROVED_BY_SrAO'
-    APPROVED_BY_R = 'APPROVED_BY_R'
-    AMOUNT_TRANSFERRED = 'AMOUNT_TRANSFERRED'
+    SUBMITTED = 'Submitted'
+    VERIFIED_BY_DA = 'Verified by Dealing Assistant'
+    APPROVED_BY_MS = 'Verified by Medical Superintendent'
+    APPROVED_BY_DR = 'Approved by Deputy Registrar'
+    APPROVED_BY_SrAO = 'Approved by Senior Audit Officer.'
+    APPROVED_BY_R = 'Approved by Registrar'
+    AMOUNT_TRANSFERRED = 'Reimbursed'
 
     def __init__(self):
         pass
@@ -18,7 +17,6 @@ STATE_CHOICES = (
     (STATE.SUBMITTED, 'Submitted by employee', 'Medical'),
     (STATE.VERIFIED_BY_DA, 'Verified by Dealing Assistant', 'Medical'),
     (STATE.APPROVED_BY_MS, 'Approved by Medical Superintendent', 'Medical'),
-    (STATE.APPROVED_BY_AR, 'Approved by Assistant Registrar', 'Medical'),
     (STATE.APPROVED_BY_DR, 'Approved by Deputy Registrar', 'Medical'),
     (STATE.APPROVED_BY_SrAO, 'Approved by Senior Audit Officer', 'Medical'),
     (STATE.APPROVED_BY_R, 'Approved by Registrar', 'Medical'),
