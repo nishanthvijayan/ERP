139d2cc25434bd4fd4776754f609d508c67451ca
Jainendra Mandavi <jainendra16mandavi@gmail.com>
2017-05-04 02:41:25 +0530

    Add folder and files structure
    Add folders along with files with static data

--- reimbursement/views/medical/state-change/state-change.py
+++ reimbursement/views/medical/state-change/state-change.py
@@ -1,8 +1,22 @@
-from django.shortcuts import render, get_object_or_404
-
-from reimbursement.models.medical.medical import Medical
+from django.shortcuts import render
+from django.http import Http404
 
 
 def state_change(request, medical_id):
-
-    return render(request, 'reimbursement/medical/index.html', context)
+    if request.user.groups.filter(name='Reimbursement_Medical_Change_State').exists():
+        if request.user.groups.filter(name='AAO_AccountsDepartment').exists():
+            pass
+        elif request.user.groups.filter(name='MS_HealthCareDepartment').exists():
+            pass
+        elif request.user.groups.filter(name='DR_AccountsDepartment').exists():
+            pass
+        elif request.user.groups.filter(name='SrAO_AuditDepartment').exists():
+            pass
+        elif request.user.groups.filter(name='R_AdministrativeDepartment').exists():
+            pass
+        elif request.user.groups.filter(name='JrAO_AccountsDepartment').exists():
+            pass
+        else:
+            raise Http404
+    else:
+        raise Http404
