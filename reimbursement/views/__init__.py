61a3d65969a42e1d3d0a868e1866a52f027d7019
Jainendra Mandavi <jainendra16mandavi@gmail.com>
2017-05-04 10:50:08 +0530

    Add url patterns for state-change and new page
    Add dynamic url for respond to request in each form display
    Add permission check to display respond to request

--- reimbursement/views/__init__.py
+++ reimbursement/views/__init__.py
@@ -4,4 +4,13 @@ from requests_pending import *
 from requests_previous import *
 
 # Medical
-from medical.index import *
+from medical.new import new
+from medical.index import show
+from medical.state_change.state_change import state_change
+# Not Required
+# from medical.state_change.role_based import generate_state_change_dealing_assistant
+# from medical.state_change.role_based import generate_state_change_medical_superintendent
+# from medical.state_change.role_based import generate_state_change_deputy_registrar
+# from medical.state_change.role_based import generate_state_change_senior_audit_officer
+# from medical.state_change.role_based import generate_state_change_registrar
+# from medical.state_change.role_based import generate_state_change_junior_accounting_officer
