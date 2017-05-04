1a2e71ea8d63a98dcc4a2c2e9963d9e111691853
Jainendra Mandavi <jainendra16mandavi@gmail.com>
2017-05-04 16:12:10 +0530

    Add state-change fucntion on medical model

--- reimbursement/views/medical/state_change/role_based/dealing_assistant.py
+++ reimbursement/views/medical/state_change/role_based/dealing_assistant.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib import messages
 from django.http import Http404
+from django.core.exceptions import PermissionDenied
+
+from django_fsm import can_proceed
 
 from reimbursement.models.medical import Medical
 
@@ -14,14 +17,22 @@ def generate_state_change_dealing_assistant(request, medical_id):
         if form.is_valid():
             if request.POST.get('VERIFIED_BY_DA', False):
                 if form.save():
-                    messages.success(request, 'Response for medical reimbursement #' + str(medical_id)
-                                     + ' successfully registered!')
+                    if not can_proceed(medical.verify_by_da):
+                        raise PermissionDenied
                     medical.verify_by_da()
+                    medical.save()
+                    messages.success(request, 'Request for medical reimbursement #' + str(medical_id)
+                                     + ' successfully approved!')
                     return redirect('reimbursement:medical-show', medical_id)
                 else:
                     messages.error(request, 'Please resolve the errors below and try again')
             if request.POST.get('REJECTED_BY_DA', False):
-                pass
+                if not can_proceed(medical.reject_by_da):
+                    raise PermissionDenied
+                medical.reject_by_da()
+                medical.save()
+                messages.success(request, 'Request for medical reimbursement #' + str(medical_id)
+                                 + ' successfully rejected!')
     else:
         form = AmountDetailForm()
     context = {
