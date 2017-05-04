fe7661c5cbe719b6537f169c6e995f5d4d119b91
Jainendra Mandavi <jainendra16mandavi@gmail.com>
2017-05-04 15:52:15 +0530

    Add reject states and transition functions

--- reimbursement/models/medical/medical.py
+++ reimbursement/models/medical/medical.py
@@ -90,6 +90,61 @@ class Medical(BaseModel):
         """
         pass
 
+    @transition(field=state, source=STATE.SUBMITTED, target=STATE.REJECTED_BY_DA)
+    def reject_by_da(self):
+        """
+        Transition function for django-fsm which changes the state of the model upon call
+        Look into the django-fsm documentation for more
+        This function may contain side-effects,
+        like updating caches, notifying users, etc.
+        :return: null
+        """
+        pass
+
+    @transition(field=state, source=STATE.VERIFIED_BY_DA, target=STATE.REJECTED_BY_MS)
+    def reject_by_ms(self):
+        """
+        Transition function for django-fsm which changes the state of the model upon call
+        Look into the django-fsm documentation for more
+        This function may contain side-effects,
+        like updating caches, notifying users, etc.
+        :return: null
+        """
+        pass
+
+    @transition(field=state, source=STATE.APPROVED_BY_MS, target=STATE.REJECTED_BY_DR)
+    def reject_by_dr(self):
+        """
+        Transition function for django-fsm which changes the state of the model upon call
+        Look into the django-fsm documentation for more
+        This function may contain side-effects,
+        like updating caches, notifying users, etc.
+        :return: null
+        """
+        pass
+
+    @transition(field=state, source=STATE.APPROVED_BY_DR, target=STATE.REJECTED_BY_SrAO)
+    def reject_by_sr_ao(self):
+        """
+        Transition function for django-fsm which changes the state of the model upon call
+        Look into the django-fsm documentation for more
+        This function may contain side-effects,
+        like updating caches, notifying users, etc.
+        :return: null
+        """
+        pass
+
+    @transition(field=state, source=STATE.APPROVED_BY_SrAO, target=STATE.REJECTED_BY_R)
+    def reject_by_r(self):
+        """
+        Transition function for django-fsm which changes the state of the model upon call
+        Look into the django-fsm documentation for more
+        This function may contain side-effects,
+        like updating caches, notifying users, etc.
+        :return: null
+        """
+        pass
+
     def __str__(self):
         return str(self.general_detail.employee.user.first_name) + ' ' \
                + str(self.general_detail.employee.user.last_name) \
--- reimbursement/models/medical/state.py
+++ reimbursement/models/medical/state.py
@@ -8,8 +8,12 @@ class STATE:
     APPROVED_BY_DR = 'Approved by Deputy Registrar'
     APPROVED_BY_SrAO = 'Approved by Senior Audit Officer.'
     APPROVED_BY_R = 'Approved by Registrar'
-    AMOUNT_TRANSFERRED = 'Reimbursed'
-    REJECT = 'Rejected'
+    AMOUNT_TRANSFERRED = 'Reimbursed',
+    REJECTED_BY_DA = 'Rejected by Dealing Assistant',
+    REJECTED_BY_MS = 'Rejected by Dealing Assistant',
+    REJECTED_BY_DR = 'Rejected by Dealing Assistant',
+    REJECTED_BY_SrAO = 'Rejected by Dealing Assistant',
+    REJECTED_BY_R = 'Rejected by Dealing Assistant'
 
     def __init__(self):
         pass
