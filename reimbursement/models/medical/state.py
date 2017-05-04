d7357e63ec7885b8c61d72515be75475d31b3117
Jainendra Mandavi <jainendra16mandavi@gmail.com>
2017-05-04 20:14:53 +0530

    Add state and permission based views for all

--- reimbursement/models/medical/state.py
+++ reimbursement/models/medical/state.py
@@ -6,13 +6,13 @@ class STATE:
     VERIFIED_BY_DA = 'Verified by Dealing Assistant'
     APPROVED_BY_MS = 'Verified by Medical Superintendent'
     APPROVED_BY_DR = 'Approved by Deputy Registrar'
-    APPROVED_BY_SrAO = 'Approved by Senior Audit Officer.'
+    APPROVED_BY_SrAO = 'Approved by Senior Audit Officer'
     APPROVED_BY_R = 'Approved by Registrar'
-    AMOUNT_TRANSFERRED = 'Reimbursed',
-    REJECTED_BY_DA = 'Rejected by Dealing Assistant',
-    REJECTED_BY_MS = 'Rejected by Dealing Assistant',
-    REJECTED_BY_DR = 'Rejected by Dealing Assistant',
-    REJECTED_BY_SrAO = 'Rejected by Dealing Assistant',
+    AMOUNT_TRANSFERRED = 'Reimbursed'
+    REJECTED_BY_DA = 'Rejected by Dealing Assistant'
+    REJECTED_BY_MS = 'Rejected by Dealing Assistant'
+    REJECTED_BY_DR = 'Rejected by Dealing Assistant'
+    REJECTED_BY_SrAO = 'Rejected by Dealing Assistant'
     REJECTED_BY_R = 'Rejected by Dealing Assistant'
 
     def __init__(self):
