8bf040eeae4516bb7da7f4e067df67d62a2486c7
Jainendra Mandavi <jainendra16mandavi@gmail.com>
2017-05-04 11:31:03 +0530

    Add ModelForm for AmountDetail
    Add base template in state-change

--- reimbursement/views/medical/state_change/role_based/dealing_assistant.py
+++ reimbursement/views/medical/state_change/role_based/dealing_assistant.py
@@ -1,11 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 
-from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
-
 from reimbursement.models.medical import Medical
-from reimbursement.models.medical.state import STATE
 
 
 def generate_state_change_dealing_assistant(request, medical_id):
-    return render(request, 'reimbursement/medical/state_change/role_based/dealing_assistant.html')
+    medical = get_object_or_404(Medical, id=medical_id)
+    context = {
+        "medical" : medical
+    }
+    return render(request, 'reimbursement/medical/state_change/role_based/dealing_assistant.html', context)
