bfedade2e8b8c87c5b424cf5361ce274fd9fc3ac
Jainendra Mandavi <jainendra16mandavi@gmail.com>
2017-05-04 13:14:28 +0530

    Display basic non-working fields in state-change page
    Add AmountDetailForm for AmountDetail model
    Add basic form for approval of Dealing Assistant

--- reimbursement/views/medical/state_change/role_based/dealing_assistant.py
+++ reimbursement/views/medical/state_change/role_based/dealing_assistant.py
@@ -1,12 +1,26 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib import messages
 from django.http import Http404
 
 from reimbursement.models.medical import Medical
 
+from reimbursement.forms.medical.amount_detail.amount_detail import AmountDetailForm
+
 
 def generate_state_change_dealing_assistant(request, medical_id):
     medical = get_object_or_404(Medical, id=medical_id)
+    if request.method == 'POST':
+        form = AmountDetailForm(data=request.POST)
+        if form.is_valid():
+            if form.save():
+                messages.success(request, 'Response for medical reimbursement #<ID> \'' + '\' successfully registered!')
+            else:
+                messages.error(request, 'Some error occurred')
+            return redirect('reimbursement:medical-show',2)
+    else:
+        form = AmountDetailForm()
     context = {
-        "medical" : medical
+        "medical": medical,
+        "form" : form
     }
     return render(request, 'reimbursement/medical/state_change/role_based/dealing_assistant.html', context)
--- reimbursement/views/medical/state_change/state_change.py
+++ reimbursement/views/medical/state_change/state_change.py
@@ -1,5 +1,9 @@
 from django.http import Http404
 
+from reimbursement.models.medical import Medical
+
+from reimbursement.models.medical.state import STATE
+
 from role_based.dealing_assistant import generate_state_change_dealing_assistant
 from role_based.medical_superintendent import generate_state_change_medical_superintendent
 from role_based.deputy_registrar import generate_state_change_deputy_registrar
@@ -9,18 +13,24 @@ from role_based.junior_accounting_officer import generate_state_change_junior_ac
 
 
 def state_change(request, medical_id):
-    if request.user.groups.filter(name='Reimbursement_Medical_Change_State').exists():
-        if request.user.groups.filter(name='AAO_AccountsDepartment').exists():
+    if Medical.objects.filter(id=medical_id).exists():
+        if request.user.groups.filter(name='AAO_AccountsDepartment').exists() \
+                and Medical.objects.filter(id=medical_id).filter(state=STATE.SUBMITTED).exists():
             return generate_state_change_dealing_assistant(request, medical_id)
-        elif request.user.groups.filter(name='MS_HealthCareDepartment').exists():
+        elif request.user.groups.filter(name='MS_HealthCareDepartment').exists() \
+                and Medical.objects.filter(id=medical_id).filter(state=STATE.VERIFIED_BY_DA).exists():
             return generate_state_change_medical_superintendent(request, medical_id)
-        elif request.user.groups.filter(name='DR_AccountsDepartment').exists():
+        elif request.user.groups.filter(name='DR_AccountsDepartment').exists() \
+                and Medical.objects.filter(id=medical_id).filter(state=STATE.APPROVED_BY_MS).exists():
             return generate_state_change_deputy_registrar(request, medical_id)
-        elif request.user.groups.filter(name='SrAO_AuditDepartment').exists():
+        elif request.user.groups.filter(name='SrAO_AuditDepartment').exists() \
+                and Medical.objects.filter(id=medical_id).filter(state=STATE.APPROVED_BY_DR).exists():
             return generate_state_change_senior_audit_officer(request, medical_id)
-        elif request.user.groups.filter(name='R_AdministrativeDepartment').exists():
+        elif request.user.groups.filter(name='R_AdministrativeDepartment').exists() \
+                and Medical.objects.filter(id=medical_id).filter(state=STATE.APPROVED_BY_SrAO).exists():
             return generate_state_change_registrar(request, medical_id)
-        elif request.user.groups.filter(name='JrAO_AccountsDepartment').exists():
+        elif request.user.groups.filter(name='JrAO_AccountsDepartment').exists() \
+                and Medical.objects.filter(id=medical_id).filter(state=STATE.APPROVED_BY_R).exists():
             return generate_state_change_junior_accounting_officer(request, medical_id)
         else:
             raise Http404
