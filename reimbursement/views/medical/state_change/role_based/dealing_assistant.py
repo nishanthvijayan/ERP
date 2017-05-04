a09f65702bf25f7631fe1afae6cc33236573fbb8
Jainendra Mandavi <jainendra16mandavi@gmail.com>
2017-05-04 15:37:38 +0530

    Add state-change on form submit

--- reimbursement/views/medical/state_change/role_based/dealing_assistant.py
+++ reimbursement/views/medical/state_change/role_based/dealing_assistant.py
@@ -12,17 +12,20 @@ def generate_state_change_dealing_assistant(request, medical_id):
     if request.method == 'POST':
         form = AmountDetailForm(data=request.POST, instance=medical.amount_detail)
         if form.is_valid():
-            if form.save():
-                messages.success(request, 'Response for medical reimbursement #' + str(medical_id)
-                                 + ' successfully registered!')
-                return redirect('reimbursement:medical-show', medical_id)
-            else:
-                messages.error(request, 'Please resolve the errors below and try again')
-                return redirect('reimbursement:medical-state-change', medical_id)
+            if request.POST.get('VERIFIED_BY_DA', False):
+                if form.save():
+                    messages.success(request, 'Response for medical reimbursement #' + str(medical_id)
+                                     + ' successfully registered!')
+                    medical.verify_by_da()
+                    return redirect('reimbursement:medical-show', medical_id)
+                else:
+                    messages.error(request, 'Please resolve the errors below and try again')
+            if request.POST.get('REJECTED_BY_DA', False):
+                pass
     else:
         form = AmountDetailForm()
     context = {
         "medical": medical,
-        "form" : form
+        "form": form
     }
     return render(request, 'reimbursement/medical/state_change/role_based/dealing_assistant.html', context)
