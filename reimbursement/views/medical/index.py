1ef7879c13a035a653415f32b04d31183bfb1342
Jainendra Mandavi <jainendra16mandavi@gmail.com>
2017-05-04 09:46:36 +0530

    Add permission checking for medical form display

--- reimbursement/views/medical/index.py
+++ reimbursement/views/medical/index.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 
 from reimbursement.models.medical.medical import Medical
 
 
 def show(request, medical_id):
     medical = get_object_or_404(Medical, id=medical_id)
-    context = {
-        'medical': medical
-    }
-    return render(request, 'reimbursement/medical/index.html', context)
+    if request.user.groups.filter(name='Reimbursement_Medical_Change_State').exists() \
+            or Medical.objects.filter(id=medical_id)\
+                    .filter(general_detail__employee__user_id=request.user.id)\
+                    .exists():
+        context = {
+            'medical': medical
+        }
+        return render(request, 'reimbursement/medical/index.html', context)
+    else:
+        raise Http404
