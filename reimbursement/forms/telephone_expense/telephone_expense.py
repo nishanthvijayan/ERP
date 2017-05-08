a7fa6efd536d90e6f6991059bd104e912cd61f0f
Jainendra Mandavi <jainendra16mandavi@gmail.com>
2017-05-08 06:05:37 +0530

    Add forms for telephone expenses

--- reimbursement/forms/telephone_expense/telephone_expense.py
+++ reimbursement/forms/telephone_expense/telephone_expense.py
@@ -1,23 +1,25 @@
-# from django import forms
-#
-# from reimbursement.models.telephone_expense.telephone_expense import TelephoneExpense
-#
-#
-# class TelephoneExpenseForm(forms.ModelForm):
-#     class Meta:
-#         model = TelephoneExpense
-#
-#     def __init__(self, *args, **kwargs):
-#         super(TelephoneExpenseForm, self).__init__(*args, **kwargs)
-#         for field in TelephoneExpense._meta.get_fields():
-#             if field.name in self.fields:
-#                 self.fields[field.name].widget.attrs = {
-#                         'class': 'form-control',
-#                         'placeholder': self.fields[field.name].help_text
-#                     }
-#
-#     def save(self, commit=True):
-#         telephone_expense = super(TelephoneExpenseForm, self).save(commit=False)
-#         if commit:
-#             telephone_expense.save()
-#         return telephone_expense
+from django import forms
+
+from reimbursement.models.telephone_expense.telephone_expense import TelephoneExpense
+
+
+class TelephoneExpenseForm(forms.ModelForm):
+    class Meta:
+        model = TelephoneExpense
+        fields = ['register_page_no', 'register_serial_no', 'amount_passed']
+
+    def __init__(self, *args, **kwargs):
+        super(TelephoneExpenseForm, self).__init__(*args, **kwargs)
+        for field in TelephoneExpense._meta.get_fields():
+            if field.name in self.fields:
+                print field.name
+                self.fields[field.name].widget.attrs = {
+                        'class': 'form-control',
+                        'placeholder': self.fields[field.name].help_text
+                    }
+
+    def save(self, commit=True):
+        telephone_expense = super(TelephoneExpenseForm, self).save(commit=False)
+        if commit:
+            telephone_expense.save()
+        return telephone_expense
