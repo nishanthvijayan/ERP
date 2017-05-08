c2bb8ac3f06605eb0bd7a33d42418747e411cceb
Nishanth Vijayan <nishanththegr8@gmail.com>
2017-05-08 22:15:32 +0530

    Pending and Previous forms should be shown correctly for users with multiple roles

--- purchase/views/purchase_general.py
+++ purchase/views/purchase_general.py
@@ -35,15 +35,27 @@ def requests_pending(request):
     """View function that renders list of purchase forms pending current user's approval."""
     current_employee = request.user.employee_set.all()[0]
 
+    pending_requests_list_hod = PurchaseIndentRequest.objects.none()
+    pending_requests_list_jao = PurchaseIndentRequest.objects.none()
+    pending_requests_list_dr = PurchaseIndentRequest.objects.none()
+
     if Department.objects.filter(hod=current_employee).exists():
         subordinates = current_employee.department.employee_set.values_list('id', flat=True)
-        pending_requests_list = PurchaseIndentRequest.objects.filter(state='Submitted', indenter_id__in=subordinates)
-    elif request.user.groups.filter(name='JrAO_AccountsDepartment').exists():
-        pending_requests_list = PurchaseIndentRequest.objects.filter(state='Approved by Head of Department')
-    elif request.user.groups.filter(name='DR_AccountsDepartment').exists():
-        pending_requests_list = PurchaseIndentRequest.objects.filter(state='Approved by Junior Accounts Officer')
-    else:
-        pending_requests_list = PurchaseIndentRequest.objects.none()
+        pending_requests_list_hod = PurchaseIndentRequest.objects.filter(
+            state='Submitted',
+            indenter_id__in=subordinates
+        )
+
+    if request.user.groups.filter(name='JrAO_AccountsDepartment').exists():
+        pending_requests_list_jao = PurchaseIndentRequest.objects.filter(state='Approved by Head of Department')
+
+    if request.user.groups.filter(name='DR_AccountsDepartment').exists():
+        pending_requests_list_dr = PurchaseIndentRequest.objects.filter(state='Approved by Junior Accounts Officer')
+
+    # Merge different forms accssible via different possible roles, take distinct and sort
+
+    pending_requests_list = (pending_requests_list_hod | pending_requests_list_jao |
+                             pending_requests_list_dr).distinct().order_by('-created_at')
 
     page = request.GET.get('page')
     paginator = Paginator(pending_requests_list, 10)
@@ -61,10 +73,13 @@ def requests_pending(request):
 def requests_previous(request):
     """View function that renders list of purchase forms previously approved by current user."""
     current_employee = request.user.employee_set.all()[0]
+    previous_requests_list_hod = PurchaseIndentRequest.objects.none()
+    previous_requests_list_jao = PurchaseIndentRequest.objects.none()
+    previous_requests_list_dr = PurchaseIndentRequest.objects.none()
 
     if Department.objects.filter(hod=current_employee).exists():
         subordinates = current_employee.department.employee_set.values_list('id', flat=True)
-        previous_requests_list = PurchaseIndentRequest.objects.filter(
+        previous_requests_list_hod = PurchaseIndentRequest.objects.filter(
             state__in=['Approved by Head of Department',
                        'Approved by Junior Accounts Officer',
                        'Approved by Deputy Registrar',
@@ -72,24 +87,25 @@ def requests_previous(request):
             indenter_id__in=subordinates
         )
 
-    elif request.user.groups.filter(name='JrAO_AccountsDepartment').exists():
+    if request.user.groups.filter(name='JrAO_AccountsDepartment').exists():
         previous_approved_ids = TransitionHistory.objects.filter(
             from_state='Approved by Head of Department'
         ).values_list('form_id', flat=True)
-        previous_requests_list = PurchaseIndentRequest.objects.filter(
+        previous_requests_list_jao = PurchaseIndentRequest.objects.filter(
             id__in=previous_approved_ids
         )
 
-    elif request.user.groups.filter(name='DR_AccountsDepartment').exists():
+    if request.user.groups.filter(name='DR_AccountsDepartment').exists():
         previous_approved_ids = TransitionHistory.objects.filter(
             from_state='Approved by Junior Accounts Officer'
         ).values_list('form_id', flat=True)
-        previous_requests_list = PurchaseIndentRequest.objects.filter(
+        previous_requests_list_dr = PurchaseIndentRequest.objects.filter(
             id__in=previous_approved_ids
         )
 
-    else:
-        previous_requests_list = PurchaseIndentRequest.objects.none()
+    # Merge different forms accssible via different possible roles, take distinct and sort
+    previous_requests_list = (previous_requests_list_hod | previous_requests_list_jao |
+                              previous_requests_list_dr).distinct().order_by('-created_at')
 
     page = request.GET.get('page')
     paginator = Paginator(previous_requests_list, 10)
