415e483d4e3ab23152c5113f83ff6b981cecbd98
Jainendra Mandavi <jainendra16mandavi@gmail.com>
2017-05-08 06:57:31 +0530

    Add settinngs for fileupload

--- erp/settings.py
+++ erp/settings.py
@@ -68,6 +68,7 @@ TEMPLATES = [
                 'django.template.context_processors.media',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
