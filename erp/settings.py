0b7c4338d6050afeeb9d4440c8ac32d30db661ad
Jainendra Mandavi <jainendra16mandavi@gmail.com>
2017-05-09 09:33:41 +0530

    Remove django_extensions

--- erp/settings.py
+++ erp/settings.py
@@ -38,8 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'django_extensions',
-
     'erp_core.apps.ErpCoreConfig',
     'home.apps.HomeConfig',
     'user_management.apps.UserManagementConfig',
